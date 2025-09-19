62f4057039eaab34810fdcd8b4d49a6ef01b77c2
rivodry <212361889+rivodry@users.noreply.github.com>
2025-09-19 17:25:59 +0530

    numbers to be taken as idents

--- interpreter/lex.go
+++ interpreter/lex.go
@@ -19,39 +19,16 @@ func ParseString(num *int, Set string) string {
 	*num = i
 	return str
 }
-func ParseNumber(nu *int, Set string) string {
-	i := *nu
-	dots := 0
-	var num string
-	for i < len(Set) && dots < 2 && (unicode.IsDigit(rune(Set[i])) || rune(Set[i]) == '.') {
-		if rune(Set[i]) == '.' {
-			dots++
-		}
 
-		num = num + string(Set[i])
-		i++
-	}
-	if num[len(num)-1] == '.' {
-		num = num[:len(num)-1]
-
-	}
-	*nu = i
-
-	return num
-}
 func Lex(Set string) []Token {
 	var Tokens []Token
 	for i := 0; i < len(Set); {
 		if unicode.IsSpace(rune(Set[i])) {
 			i++
 			continue
-		} else if unicode.IsLetter(rune(Set[i])) || Set[i] == '_' {
+		} else if unicode.IsLetter(rune(Set[i])) || unicode.IsDigit(rune(Set[i])) || Set[i] == '_' {
 			str := ParseString(&i, Set)
 			Tokens = append(Tokens, Token{"IDENT", str})
-		} else if unicode.IsDigit(rune(Set[i])) {
-			num := ParseNumber(&i, Set)
-			Tokens = append(Tokens, Token{"NUMBER", num})
-
 		} else {
 			i++
 		}
